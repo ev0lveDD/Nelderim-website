884471c0daa64afb65b2671bbf073eb70624a48c
ev0lveDD <102914983+ev0lveDD@users.noreply.github.com>
2022-05-27 20:17:34 +0200

    Register form added
    Register form added

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ function toggleMenu(event) {
   event.classList.toggle("open");
   document.querySelector(".navbar").classList.toggle("invisible");
 }
+
+
+function toggleForm() {
+  document.querySelector(".register").classList.toggle("invisible-form");
+}
